e38e1532d40b40bbf8c1c3962013b3b61d0fc395
James Moon <mooniker@gmail.com>
2016-04-14 17:41:29 -0400

    Remove old version of tests pre-refactoring to use CSV

--- test.py
+++ test.py
@@ -1,24 +1,5 @@
 from indivisible import is_prime, get_how_many_prime_numbers
 
-# # check some basic prime numbers
-# assert is_prime(3) == True
-# assert is_prime(4) == False
-# assert is_prime(7) == True
-# assert is_prime(9) == False
-# assert is_prime(10) == False
-# assert is_prime(11) == True
-# assert is_prime(95) == False
-# assert is_prime(101) == True
-#
-# first_168_prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
-#
-# assert get_how_many_prime_numbers(168) == first_168_prime_numbers
-#
-# for i in range(1, 168 + 1):
-#     assert get_how_many_prime_numbers(i) == first_168_prime_numbers[:i]
-#
-# print 'All passed.'
-
 # Get Wikipedia-sourced/copy-pasted prime numbers against which to test
 with open('prime_numbers.csv', 'r') as f:
     read_data = f.read()
