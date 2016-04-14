ecb633c26674a26262f101043cfdbb980785b40e
James Moon <mooniker@gmail.com>
2016-04-14 15:52:05 -0400

    Fix bug that allowed 9 to evaluate true as a prime number

--- indivisible.js
+++ indivisible.js
@@ -2,11 +2,11 @@
 function isPrime(n) {
   // Check to make sure number isn't even and isn't less than 2.
   if (n % 2 === 0 && n > 2)
-    return false
+    return false;
   // Check to see if every odd number from 3 to one greater than
   // the square root of n evenly divide into n. If any of them
   // do so, return False.
-  for (var i = 3; i < Math.ceil(Math.sqrt(n)); i += 2) {
+  for (var i = 3; i < Math.ceil(Math.sqrt(n)) + 1; i += 2) {
     if (n % i === 0)
       return false;
   } // Otherwise
@@ -16,13 +16,12 @@ function isPrime(n) {
 function getHowManyPrimeNumbers(n) {
   var i = 2;
   var results = [];
-  while (true) {
+  while (results.length < n) {
     if (isPrime(i))
       results.push(i);
-    if (results.length >= n)
-      return results;
     i++;
   }
+  return results;
 }
 
 module.exports = {
